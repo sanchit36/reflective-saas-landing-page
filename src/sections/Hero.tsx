bb65438e8037bfaed251e691820109cd2e5cbb91
sanchit bhadgal <sanchitbhadgal36@gmail.com>
2025-06-05 13:08:57 +0530

    Enhance sections with framer-motion for improved animations and scrolling effects

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,12 +1,24 @@
+"use client";
 import ArrowRightIcon from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
 import cylinderImage from "@/assets/cylinder.png";
 import noodleImage from "@/assets/noodle.png";
-import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const Hero = () => {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE)] overflow-x-clip">
+    <section
+      ref={heroRef}
+      className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE)] overflow-x-clip"
+    >
       <div className="container">
         <div className="md:flex md:items-center">
           <div className="md:w-[478px]">
@@ -27,24 +39,40 @@ export const Hero = () => {
             </div>
           </div>
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image
-              src={cogImage}
+            <motion.img
+              src={cogImage.src}
               alt="Cog Image"
               className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+              animate={{
+                translateY: [-30, 30],
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "mirror",
+                duration: 2,
+                ease: "easeInOut",
+              }}
             />
-            <Image
-              src={cylinderImage}
+            <motion.img
+              src={cylinderImage.src}
               width={220}
               height={220}
               alt="Cylinder Image"
               className="hidden md:block -top-8 -left-32 absolute"
+              style={{
+                translateY: translateY,
+              }}
             />
-            <Image
-              src={noodleImage}
+            <motion.img
+              src={noodleImage.src}
               width={220}
               height={220}
               alt="Noodle Image"
-              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+              className="hidden lg:block absolute top-[524px] left-[448px]"
+              style={{
+                translateY: translateY,
+                rotate: 30,
+              }}
             />
           </div>
         </div>
--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,5 +1,7 @@
+"use client";
 import CheckIcon from "@/assets/check.svg";
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
 
 const pricingTiers = [
   {
@@ -92,9 +94,18 @@ export const Pricing = () => {
                   </h3>
                   {popular && (
                     <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
-                      <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3bFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                      <motion.span
+                        animate={{ backgroundPositionX: "100%" }}
+                        transition={{
+                          repeat: Infinity,
+                          ease: "linear",
+                          repeatType: "loop",
+                          duration: 1,
+                        }}
+                        className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3bFFFF,#DD7DDF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3bFFFF)] [background-size:200%] text-transparent bg-clip-text font-medium"
+                      >
                         Popular
-                      </span>
+                      </motion.span>
                     </div>
                   )}
                 </div>
